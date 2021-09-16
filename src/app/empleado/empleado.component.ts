da34c910f219f7760dc667b9352656d210e54fd8
suarezt3 <ey_der19@hotmail.com>
2021-09-15 21:14:18 -0500

    Se avanza  en el curso hasat el video 8

--- src/app/empleado/empleado.component.ts
+++ src/app/empleado/empleado.component.ts
@@ -7,6 +7,19 @@ import { Component, OnInit } from '@angular/core';
 })
 export class EmpleadoComponent implements OnInit {
 
+  nombre: string = "Eyder";
+  apellido: string = "Suarez"
+  edad: number = 10;
+  //private empresa: string = "Suarez Corporation";
+
+  // llamaEmpresa(value:String){
+
+  // }
+
+  // getEmpresa () { //este metodo se utiliza para poder acceder a una propiedad cuando la protegemos con el metodo private
+  //   return this.empresa;
+  // }
+
   constructor() { }
 
   ngOnInit(): void {
